4d44b02fb4ac5f4cc3f1c78f954c6d0cb89b2ff8
Douglas Faria <hfsdouglas@gmail.com>
2025-01-16 21:32:39 -0300

    fix: font roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,15 +5,13 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
-
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
       </Head>
