cf382a7e613fce7054883957863029fce83b436a
Douglas Faria <hfsdouglas@gmail.com>
2025-01-17 22:03:13 -0300

    feat: add componets and styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import { globalStyles } from "@/styles/global";
+import Image from "next/image";
 import type { AppProps } from "next/app";
+import { globalStyles } from "@/styles/global";
 
 globalStyles();
 
+import logoImg from "../assets/logo.svg";
+
+import { Container, Header } from "@/styles/pages/app";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
