8c47449f355798d301e9a4b8334c4b5e0c10cf7f
Douglas Faria <hfsdouglas@gmail.com>
2025-01-17 22:53:20 -0300

    fix: global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import type { AppProps } from "next/app";
 import { globalStyles } from "@/styles/global";
 
-globalStyles();
-
 import logoImg from "../assets/logo.svg";
 
 import { Container, Header } from "@/styles/pages/app";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
